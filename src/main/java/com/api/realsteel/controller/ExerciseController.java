c4321e57f71368b571e9f6147cf20df08340e3f7
fromerog03 <fernandoromero>
2026-03-19 14:18:29 +0100

    VAMOS

--- src/main/java/com/api/realsteel/controller/ExerciseController.java
+++ src/main/java/com/api/realsteel/controller/ExerciseController.java
@@ -1,14 +1,24 @@
 package com.api.realsteel.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.api.realsteel.dto.ExerciseRequest;
 import com.api.realsteel.entity.ExerciseEntity;
 import com.api.realsteel.service.ExerciseService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/exercises")
@@ -21,7 +31,7 @@ public class ExerciseController {
         this.exerciseService = exerciseService;
     }
 
-    @GetMapping
+    @GetMapping("/getAll")
     public List<ExerciseEntity> getExercises() {
         return exerciseService.getAllExercises();
     }
