160c9f0773ad49a5b9d02c5fabd860d3fa679022
fromerog03 <fernandoromero>
2026-03-19 14:00:38 +0100

    furubi

--- src/main/java/com/api/realsteel/controller/UserController.java
+++ src/main/java/com/api/realsteel/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.api.realsteel.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.api.realsteel.dto.UserRequest;
 import com.api.realsteel.entity.UserEntity;
 import com.api.realsteel.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -21,7 +31,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<UserEntity> createUser(@Valid @RequestBody UserRequest userRequest) {
         UserEntity user = new UserEntity();
         user.setUserId(userRequest.getUserId());
